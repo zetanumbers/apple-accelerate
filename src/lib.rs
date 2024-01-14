4bd6ffc3894ca37ffe78adaab67180fe27771f64
Daria Sukhonina <dariasukhonina@gmail.com>
2024-01-14 22:41:27 +0300

    add dot_product and distance_squared

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,30 @@
 mod dft;
-mod raw;
+pub mod raw;
+
+use std::mem;
 
 pub use dft::*;
+
+#[track_caller]
+pub fn dot_product<T: raw::Scalar>(a: &[T], b: &[T]) -> T {
+    let n = a.len();
+    assert_eq!(n, b.len(), "input slices must have equal lengths");
+    let n = n.try_into().expect("input slice length is too large");
+    let mut out = mem::MaybeUninit::uninit();
+    unsafe {
+        T::dot_product(a.as_ptr(), 1, b.as_ptr(), 1, out.as_mut_ptr(), n);
+        out.assume_init()
+    }
+}
+
+#[track_caller]
+pub fn distance_squared<T: raw::Scalar>(a: &[T], b: &[T]) -> T {
+    let n = a.len();
+    assert_eq!(n, b.len(), "input slices must have equal lengths");
+    let n = n.try_into().expect("input slice length is too large");
+    let mut out = mem::MaybeUninit::uninit();
+    unsafe {
+        T::distance_squared(a.as_ptr(), 1, b.as_ptr(), 1, out.as_mut_ptr(), n);
+        out.assume_init()
+    }
+}
