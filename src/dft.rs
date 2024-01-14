1e53d03e39af0d6c50666b6d424591f095afd3a8
Daria Sukhonina <dariasukhonina@gmail.com>
2024-01-14 22:29:38 +0300

    Modularize crate, renamings and add distance_squared raw API

---
+++ src/dft.rs
@@ -0,0 +1,69 @@
+use std::{marker::PhantomData, ptr};
+
+use apple_sys::Accelerate;
+use num_complex::Complex;
+
+use crate::raw::Scalar;
+
+pub struct DftSetup<T: Scalar> {
+    raw: ptr::NonNull<T::DftInterleavedSetupStruct>,
+    len: usize,
+    _marker: PhantomData<T>,
+}
+
+impl<T: Scalar> Drop for DftSetup<T> {
+    fn drop(&mut self) {
+        unsafe { T::dft_destroy_interleaved_setup(self.raw) }
+    }
+}
+
+impl<T: Scalar> DftSetup<T> {
+    pub fn new(len: usize, direction: FftDirection) -> Result<Self, NewDftSetupError> {
+        Ok(Self {
+            raw: ptr::NonNull::new(unsafe {
+                T::dft_create_interleaved_setup(
+                    ptr::null_mut(),
+                    len.try_into().unwrap(),
+                    direction as _,
+                    false,
+                )
+            })
+            .ok_or(NewDftSetupError(()))?
+            .cast(),
+            len,
+            _marker: PhantomData,
+        })
+    }
+
+    pub fn execute(&self, input: &[Complex<T>], output: &mut [Complex<T>]) {
+        assert_eq!(self.len, input.len());
+        assert_eq!(self.len, output.len());
+        unsafe { T::dft_execute_interleaved_setup(self.raw, input.as_ptr(), output.as_mut_ptr()) }
+    }
+
+    pub fn execute_in_place(&self, data: &mut [Complex<T>]) {
+        assert_eq!(self.len, data.len());
+        let ptr = data.as_mut_ptr();
+        unsafe { T::dft_execute_interleaved_setup(self.raw, ptr, ptr) }
+    }
+}
+
+#[repr(i32)]
+#[non_exhaustive]
+pub enum FftDirection {
+    Forward = Accelerate::FFT_FORWARD,
+    Inverse = Accelerate::FFT_INVERSE,
+}
+
+// Errors
+
+#[derive(Debug)]
+pub struct NewDftSetupError(());
+
+impl std::fmt::Display for NewDftSetupError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        "len value is unsupported or too large".fmt(f)
+    }
+}
+
+impl std::error::Error for NewDftSetupError {}
---
+++ src/raw.rs
@@ -0,0 +1,107 @@
+// Scalar trait
+
+use std::ptr;
+
+use apple_sys::Accelerate;
+use num_complex::Complex;
+
+#[allow(clippy::missing_safety_doc)]
+pub unsafe trait Scalar: Sized {
+    type DftInterleavedSetupStruct;
+
+    unsafe fn dft_create_interleaved_setup(
+        previous: *mut Self::DftInterleavedSetupStruct,
+        length: Accelerate::vDSP_Length,
+        direction: Accelerate::vDSP_DFT_Direction,
+        real_to_complex: Accelerate::vDSP_DFT_RealtoComplex,
+    ) -> *mut Self::DftInterleavedSetupStruct;
+
+    unsafe fn dft_execute_interleaved_setup(
+        setup: ptr::NonNull<Self::DftInterleavedSetupStruct>,
+        input: *const Complex<Self>,
+        output: *mut Complex<Self>,
+    );
+
+    unsafe fn dft_destroy_interleaved_setup(setup: ptr::NonNull<Self::DftInterleavedSetupStruct>);
+
+    unsafe fn distance_squared(
+        a: *const Self,
+        stride_a: Accelerate::vDSP_Stride,
+        b: *const Self,
+        stride_b: Accelerate::vDSP_Stride,
+        c: *mut Self,
+        n: Accelerate::vDSP_Length,
+    );
+}
+
+unsafe impl Scalar for f32 {
+    type DftInterleavedSetupStruct = Accelerate::vDSP_DFT_Interleaved_SetupStruct;
+
+    unsafe fn dft_create_interleaved_setup(
+        previous: *mut Self::DftInterleavedSetupStruct,
+        length: Accelerate::vDSP_Length,
+        direction: Accelerate::vDSP_DFT_Direction,
+        real_to_complex: Accelerate::vDSP_DFT_RealtoComplex,
+    ) -> *mut Self::DftInterleavedSetupStruct {
+        Accelerate::vDSP_DFT_Interleaved_CreateSetup(previous, length, direction, real_to_complex)
+    }
+
+    unsafe fn dft_execute_interleaved_setup(
+        setup: ptr::NonNull<Self::DftInterleavedSetupStruct>,
+        input: *const Complex<Self>,
+        output: *mut Complex<Self>,
+    ) {
+        Accelerate::vDSP_DFT_Interleaved_Execute(setup.as_ptr(), input.cast(), output.cast())
+    }
+
+    unsafe fn dft_destroy_interleaved_setup(setup: ptr::NonNull<Self::DftInterleavedSetupStruct>) {
+        Accelerate::vDSP_DFT_Interleaved_DestroySetup(setup.as_ptr())
+    }
+
+    unsafe fn distance_squared(
+            a: *const Self,
+            stride_a: Accelerate::vDSP_Stride,
+            b: *const Self,
+            stride_b: Accelerate::vDSP_Stride,
+            c: *mut Self,
+            n: Accelerate::vDSP_Length,
+        ) {
+        Accelerate::vDSP_distancesq(a, stride_a, b, stride_b, c, n)
+    }
+}
+
+unsafe impl Scalar for f64 {
+    type DftInterleavedSetupStruct = Accelerate::vDSP_DFT_Interleaved_SetupStructD;
+
+    unsafe fn dft_create_interleaved_setup(
+        previous: *mut Self::DftInterleavedSetupStruct,
+        length: Accelerate::vDSP_Length,
+        direction: Accelerate::vDSP_DFT_Direction,
+        real_to_complex: Accelerate::vDSP_DFT_RealtoComplex,
+    ) -> *mut Self::DftInterleavedSetupStruct {
+        Accelerate::vDSP_DFT_Interleaved_CreateSetupD(previous, length, direction, real_to_complex)
+    }
+
+    unsafe fn dft_execute_interleaved_setup(
+        setup: ptr::NonNull<Self::DftInterleavedSetupStruct>,
+        input: *const Complex<Self>,
+        output: *mut Complex<Self>,
+    ) {
+        Accelerate::vDSP_DFT_Interleaved_ExecuteD(setup.as_ptr(), input.cast(), output.cast())
+    }
+
+    unsafe fn dft_destroy_interleaved_setup(setup: ptr::NonNull<Self::DftInterleavedSetupStruct>) {
+        Accelerate::vDSP_DFT_Interleaved_DestroySetupD(setup.as_ptr())
+    }
+
+    unsafe fn distance_squared(
+            a: *const Self,
+            stride_a: Accelerate::vDSP_Stride,
+            b: *const Self,
+            stride_b: Accelerate::vDSP_Stride,
+            c: *mut Self,
+            n: Accelerate::vDSP_Length,
+        ) {
+        Accelerate::vDSP_distancesqD(a, stride_a, b, stride_b, c, n)
+    }
+}
